a27f61bc3a6cbb987b547ae45932dd0761a5a9dc
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2020-08-18 04:37:54 +0530

    Update PrimeNumber.java
    In this code, the check for a given input whether it is a prime number or not is generated in O(sqrt(n)) time complexity.

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,18 +1,29 @@
 
 public class PrimeNumber {
-public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-  System.out.println("Enter number to test ");
-  int number = sc.nextInt();
-  System.out.print(isPrime(number));
-}
+  
+// In this code, the check for a given input whether it is a prime number or not is 
+// generated in O(sqrt(n)) time complexity.
+  
+// It is evident that the factor of a prime number can't exceed the sqrt(n),
+// so instead of iterating till the n-1, by iterating till sqrt(n), the time 
+// complexity is reduced 
+  
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter number to test ");
+      int number = sc.nextInt();
+      System.out.print(isPrime(number));
+  }
 
 public static boolean isPrime(int number){
-      if(number <= 1)
-          return false;
-
+  //corner case 
+  if(number <= 1)
+    return false;
+  
   //instead of iterating index till the number, we 
-  //can iterate it over till sqrt(number) thus reducing the time complexity
+  //are iterating it over till sqrt(number) thus reducing the time complexity
+  //from O(n) to O(sqrt(n))
+ 
   for(int i=2; i< Math.sqrt(number); i++){
           if(number%i==0)
               return false;
