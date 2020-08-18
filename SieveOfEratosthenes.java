0c8dd2213102e514b10ec00454daf7b2d0659b9c
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2020-08-18 13:29:06 +0530

    Create SieveOfEratosthenes.java
    This program focuses on finding prime numbers to a given inputted range with max 100000 limit in a O(n.log(n)) time

---
+++ SieveOfEratosthenes.java
@@ -0,0 +1,43 @@
+public class SieveOfEratosthenes{
+    
+    /*
+    This program focuses on finding prime numbers to a given 
+    inputted range with max 100000 limit in a O(n.log(n)) time.
+    
+    isPrime is a boolean array that holds which index or number is prime,
+    and which isnt 
+    
+    nMax is the length declared for isPrime[] array
+    
+    */
+    static int nMax = 100001;
+    static Boolean[] isPrime = new Boolean[nMax];
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        //sieve will run till the input number
+        sieveOfEratosthenes(number);
+        
+        for(int i=2; i<= number; i++){
+            if(isPrime[i])
+                System.out.print(i+ " ");
+        }
+    }
+        
+    
+    //mark all the indexes till the number with true
+    public static void sieveOfEratosthenes(int number){
+     for(int i=2; i<=number; i++)
+            isPrime[i] = true;
+    //  iterate i till number/2 as the multiple of i is greater than or 
+    //  equal to i*2
+    for(int i=2; i<=number/2; i++)
+            if(isPrime[i])
+                // mark the multiples of i as false, so that only those values which are 
+                // prime resides in the memory
+                for(int k = i*2; k<=number; k+=i)
+                    isPrime[k] = false;
+    }
+    
+    }
