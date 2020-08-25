8f0a8b4134655dc5c0027aeef55a473ca4ea521f
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2020-08-26 02:18:51 +0530

    Create FrequencyArray.java

---
+++ FrequencyArray.java
@@ -0,0 +1,30 @@
+public class FrequencyArray{
+    static int Nmax = 1001;
+    static int[] fr = new int[Nmax];
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[Nmax];
+        int n;
+        
+        n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            a[i] = sc.nextInt();
+        }
+        
+        System.out.print(freqCount(a,n));
+    }
+    
+    public static int freqCount(int a[], int n){
+        
+        int counter=0;
+        for(int i=1; i<= n;i++){
+            if(fr[a[i]]==0)
+                counter++;
+            fr[a[i]]+= 1;
+        }
+        return counter;
+        
+    }
+}
