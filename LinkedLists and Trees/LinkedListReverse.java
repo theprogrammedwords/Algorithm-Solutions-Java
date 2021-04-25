a4358f67945f7288e4e40c58e1333387864f9570
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2021-04-26 00:06:51 +0530

    Create LinkedListReverse.java

---
+++ LinkedLists and Trees/LinkedListReverse.java
@@ -0,0 +1,33 @@
+//Reverse the given linked list.
+
+import crio.ds.List.ListNode;
+import java.util.*;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+class Solution {
+
+    public ListNode linkedListReverse(ListNode head) {
+    ListNode curr = head;
+    ListNode prev = null;
+    ListNode next = null;
+
+    while(curr != null){
+        next= curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+    return head;
+
+    }
+};
