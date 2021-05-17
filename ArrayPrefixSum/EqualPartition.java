ea34d826844523c4d452273d72bd82c9550203cf
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2021-05-17 23:37:56 +0530

    Update EqualPartition.java

--- ArrayPrefixSum/EqualPartition.java
+++ ArrayPrefixSum/EqualPartition.java
@@ -64,6 +64,8 @@ class EqualPartition {
         }
         
 
+        
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n-1; j++){
                 if(lsum[j] == rsum[i] && arr[j+1] != rsum[i]){
