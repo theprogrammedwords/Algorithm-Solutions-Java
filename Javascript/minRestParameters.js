c909ae4c43c93ecc7c8646f2074ccdf26698c1f0
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2021-06-28 18:19:30 +0530

    Create minRestParameters.js

---
+++ Javascript/minRestParameters.js
@@ -0,0 +1,8 @@
+//Create and complete a function minValue() which should return the minimum value of all arguments passed into it.
+
+
+function minValue(...restArguments){
+    return Math.min(...restArguments)
+}
+
+module.exports = minValue;
